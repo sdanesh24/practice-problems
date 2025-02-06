212a4a592756f692087a4f2d752658db7c875bd4
sdanesh24 <sdanesh@ucsd.edu>
2025-02-05 19:48:51 -0500

    Objects and classes test

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,49 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
-        // need to store each of the three inputs as a list??
+        LargestNumberUtilities largestNumberUtilities = new LargestNumberUtilities();
+        SmallestNumberUtilities smallestNumberUtilities = new SmallestNumberUtilities();
+
+        // Create a variable to store an integer
+        // Confused about variable access between methods...
+        // is it the same largest variable in main and LargestNumberUtilities or two different ones? use a getter here?
+
+        int largest = 5;
+        largest = 6;
+        int smallest;
+
+        // need to store each of the three inputs as a list?? - Safa OG comment
         int [] inputs = new int [3];
-        for(int i = 0; i <3; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println("Enter a number: ");
             inputs[i] = scanner.nextInt();
         }
-        if(inputs[0] > inputs [1] && inputs[0] > inputs[2]) {
-            System.out.println("The largest number is: " + inputs[0]);
-        } if(inputs[1] > inputs[0] && inputs[1] > inputs[2]) {
-            System.out.println("The largest number is :" + inputs[1]);
-        } if(inputs[2] > inputs[0] && inputs[2] > inputs[1]) {
-            System.out.println("The largest number is: " + inputs[2]);
-        } else {
-            return;
-        }
+
+        // Call a method to get the largest number AND return it and save that value
+        // creating an instance of the largest number utilities class to call the getLargestNumber method on bc the class is static?
+
+        largest = largestNumberUtilities.getLargestNumber(inputs);
+        smallest = smallestNumberUtilities.getSmallestNumber(inputs);
+
+        // Take that return value
+        // Create a method in LargestNumberUtilities called 'multiplyNumberBy3(capture)'
+        // Store the result of that
+        // Print that
+
+        largestNumberUtilities.multiplyNumberBy3(largest);
+
+        largestNumberUtilities.isLargestPrime(largest);
+
+        smallestNumberUtilities.numberPlusAge(smallest);
+        
+
         scanner.close();
     }
+
+    // Create a method to get the largest number
+        // LargestNumberUtilities
 }
---
+++ src/SmallestNumberUtilities.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SmallestNumberUtilities {
+    public int getSmallestNumber(int [] inputs){
+        int smallest = Math.min(inputs[0], Math.min(inputs[1], inputs[2]));
+        System.out.println("The smallest number is: " + smallest);
+        return smallest;
+    }
+
+    //can different methods have the same name if they're in different classes?
+    public void multiplyNumberBy3(int smallest){
+        int smallestTripled = smallest * 3;
+        System.out.println(smallestTripled);
+    }
+
+    public void divideNumberBy3(int smallest){
+        int smallestThird = smallest / 3;
+        System.out.println(smallestThird);
+    }
+
+    public void isSmallestPrime(int smallest){
+        List<Integer> division_Results = new ArrayList<>();
+
+        for(int i = 1; i <= smallest; i++)
+        {
+            int result = (smallest % i);
+
+            if(result == 0)
+            {
+                division_Results.add(i);
+            }
+        }
+
+        if(division_Results.size() == 2)
+        {
+            System.out.println(smallest + " is a prime number!");
+        }
+        else
+        {
+            System.out.println(smallest + " is not a prime number");
+        }
+    }
+
+    public void numberPlusAge(int smallest){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int smallestPlusAge = smallest + age;
+        System.out.println(smallest + "+" + age + " is: " + smallestPlusAge);
+    }
+}
