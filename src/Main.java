d3fab1c21dfe3d0ff8ac59543bd63d5e35c4cd49
sdanesh24 <sdanesh@ucsd.edu>
2025-04-02 14:51:30 -0400

    Updated withe .reverse() StringBuilder method

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
             System.out.println("Enter a word: ");
             String input = scanner.next();
             // creating the new empty variable to append each indexed character to
-            StringBuilder inverse = new StringBuilder();
-            for(int i = input.length()-1; i >= 0; i--){
-                inverse = inverse.append(input.charAt(i));
-            }
+            StringBuilder reverse = new StringBuilder(input).reverse();
+//            for(int i = input.length()-1; i >= 0; i--){
+//                inverse = inverse.append(input.charAt(i));
+//            }
             // how to debug and see what is being stored in the inverse variable at this stage?
-            System.out.println("Reversed word: " + inverse);
-            if(input.equals(inverse.toString())){
+            System.out.println("Reversed word: " + reverse);
+            if(input.equals(reverse.toString())){
                 System.out.println(input + " is a palindrome!");
             } else {
                 System.out.println(input + " is not a palindrome.");
